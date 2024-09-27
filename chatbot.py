22a9592566e961d27eb2093ac2785f777a028457
Vanessa Ezeh <vanessaezeh8@gmail.com>
2024-09-26 21:57:42 -0500

    Loaded the pretrained data

--- chatbot.py
+++ chatbot.py
@@ -8,9 +8,9 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 
-lemmatizer = WordNetLemmatizer(0)
+lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('/Users/vanessa/save-mate/intents.json').read())
 words = pickle.load(open('word.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 
@@ -21,11 +21,11 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
-def bag_of_sentence(sentence):
+def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
-        for i in enumerate(words):
+        for i, word in enumerate(words):
             if word == w:
                bag[i] = 1
     return np.array(bag)
@@ -35,5 +35,31 @@ def predict_class(sentence):
     res = model.predict(np.array([bow]))[0]
 
     ERROR_THRESHOLD = 0.25
-    results = []
+    results = [[i,r]for i , r in enumerate(res) if r>ERROR_THRESHOLD] 
+    results.sort(key = lambda x:x, reverse = True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return return_list
+
+def get_response(intents_list, intents_json):
+    list_of_intents = intents_json['intents']
+    tag = intents_list[0]['intent']  
+   
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice (i['responses'])
+            break
+    return result
+    
+    return result
+print("Great!Bot is Running!")
+
+while True:
+    message = input("")
+    ints = predict_class (message)
+    res = get_response (ints, intents)
+    print (res)
+
+
     
